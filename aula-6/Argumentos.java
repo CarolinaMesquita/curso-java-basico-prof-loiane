1175cae71a60cecceed78f204367bc22ff152bab
Carolina <carolti2013@gmail.com>
2015-04-21 21:49:31 -0300

    add aula 6

---
+++ aula-6/Argumentos.java
@@ -0,0 +1,6 @@
+public class Argumentos{
+		public static void main(String [] args){
+
+			System.out.println("Digite o nome: " + args[0]);
+	}
+}
---
+++ aula-6/Argumentos2.java
@@ -0,0 +1,6 @@
+public class Argumentos2{
+		public static void main(String [] args){
+
+			System.out.println("Java é: " + args[0]);
+	}
+}
